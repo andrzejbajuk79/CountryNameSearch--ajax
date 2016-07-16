4bd578b8877999f93cb11b6d380f0434b8a76963
ptasiek3003 <ptasiek3003@gmail.com>
2016-07-16 12:06:34 +0200

    third part

--- script.js
+++ script.js
@@ -61,5 +61,27 @@ $(document).ready(function() {
 	getQuote();
 	$('.trigger').click(function() {
 		getQuote();
-	})
+	});
 });
+
+//cwiczenie 3
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+$('#search').click(searchCountries);
+function searchCountries() {
+ 	var countryName = $('#country-name').val();
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+function showCountriesList(resp) {
+  	countriesList.empty();
+	resp.forEach(function(item) {
+   		$('<li>').text(item.name).appendTo(countriesList);
+	});
+}
+
